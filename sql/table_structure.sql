1d601b3b626d86ba159760f70865e0e46e66227f
ohkabe fumiya <fumiya.okabe@rooter.co.jp>
2020-10-19 07:30:21 +0900

    [add] テーブル作成sqlの追加

---
+++ sql/table_structure.sql
@@ -0,0 +1,83 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 5446
+#
+# https://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.24)
+# Database: similar_article_search
+# Generation Time: 2020-10-18 22:28:05 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table articles
+# ------------------------------------------------------------
+
+CREATE TABLE `articles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `html_id` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT '0',
+  `title` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `writer_name` varchar(255) DEFAULT NULL,
+  `writer_link` text,
+  `summary` text,
+  `body` text,
+  `article_published_at` date DEFAULT NULL,
+  `article_modified_at` date DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table htmls
+# ------------------------------------------------------------
+
+CREATE TABLE `htmls` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url_id` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT '0',
+  `html` longtext,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table urls
+# ------------------------------------------------------------
+
+CREATE TABLE `urls` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` int(11) DEFAULT '0',
+  `url` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `views` int(11) DEFAULT NULL,
+  `loves` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
