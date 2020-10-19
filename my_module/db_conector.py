762352ea9982a3a089e148cf532a2d8a728a00d7
webapp_server_for_portfolio <webapp_server_for_portfolio>
2020-10-19 16:56:51 +0000

    [add] 開発環境と本番環境でDB接続先切り替え

--- my_module/db_conector.py
+++ my_module/db_conector.py
@@ -1,29 +1,30 @@
 from orator import DatabaseManager, Model
 import os
 
-#config = {
-#    'mysql': {
-#        'driver': 'mysql',
-#        'host': 'localhost',
-#        'database': 'similar_article_search',
-#        'user': 'root',
-#        'charset': 'utf8mb4',
-#        'password': '',
-#        'prefix': ''
-#    }
-#}
-
-config = {
-    'mysql': {
-        'driver': 'mysql',
-        'host': os.environ["DB_HOSTNAME"],
-        'database': 'similar_article_search',
-        'user': 'admin',
-        'charset': 'utf8mb4',
-        'password': os.environ["DB_PASSWORD"],
-        'prefix': ''
+if os.environ["ENVIROMENT"] == "product":
+    config = {
+        'mysql': {
+            'driver': 'mysql',
+            'host': os.environ["DB_HOSTNAME"],
+            'database': 'similar_article_search',
+            'user': 'admin',
+            'charset': 'utf8mb4',
+            'password': os.environ["DB_PASSWORD"],
+            'prefix': ''
+        }
     }
-}
+else:
+    config = {
+        'mysql': {
+            'driver': 'mysql',
+            'host': 'localhost',
+            'database': 'similar_article_search',
+            'user': 'root',
+            'charset': 'utf8mb4',
+            'password': '',
+            'prefix': ''
+        }
+    }
 
 db = DatabaseManager(config)
 Model.set_connection_resolver(db)
