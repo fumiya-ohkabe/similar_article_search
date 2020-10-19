5777a5c8c5b107ca92f62704c8d3dd30d5aa1583
webapp_server_for_portfolio <webapp_server_for_portfolio>
2020-10-19 16:48:59 +0000

    [add]本番環境でのDB接続設定追加

--- my_module/db_conector.py
+++ my_module/db_conector.py
@@ -1,13 +1,26 @@
 from orator import DatabaseManager, Model
+import os
+
+#config = {
+#    'mysql': {
+#        'driver': 'mysql',
+#        'host': 'localhost',
+#        'database': 'similar_article_search',
+#        'user': 'root',
+#        'charset': 'utf8mb4',
+#        'password': '',
+#        'prefix': ''
+#    }
+#}
 
 config = {
     'mysql': {
         'driver': 'mysql',
-        'host': 'localhost',
+        'host': os.environ["DB_HOSTNAME"],
         'database': 'similar_article_search',
-        'user': 'root',
+        'user': 'admin',
         'charset': 'utf8mb4',
-        'password': '',
+        'password': os.environ["DB_PASSWORD"],
         'prefix': ''
     }
 }
